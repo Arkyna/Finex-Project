4ee654f833b3effcc7d9afd074e2b4e8f1e22f7a
ArkynaSource <jhoelhex6@gmail.com>
2024-05-05 19:05:33 +0700

    adding more things

--- initialize.py
+++ initialize.py
@@ -1,5 +1,7 @@
 import pygame as pgm
+import json
 from bin.enemy import Enemy
+from bin.world import World
 
 #initialisasi
 pgm.init()
@@ -7,8 +9,11 @@ pgm.init()
 #create clock
 clock = pgm.time.Clock()
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+ROWS = 15
+COLS = 15
+TILE_SIZE = 64
+SCREEN_WIDTH = TILE_SIZE*COLS
+SCREEN_HEIGHT = TILE_SIZE*ROWS
 FPS = 60
 
 #creating window game
@@ -16,19 +21,24 @@ screen = pgm.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pgm.display.set_caption("TDEFENSE")
 
 #load image
+#map
+map_image = pgm.image.load('assets\images\map\level_1.png').convert_alpha()
+
+#enemies
 enemy_image = pgm.image.load('assets\images\monsters\enemy1.png').convert_alpha()
+
+#load json data for level
+with open('bin\levels\level1.tmj') as file:
+    world_data = json.load(file)
+
+#create world
+world = World(world_data, map_image)
+world.process_data()
 
 #creating groups
 enemy_groups = pgm.sprite.Group()
 
-waypoints = [
-    (100, 100),
-    (250, 250),
-    (280, 180),
-    (90, 200)
-]
-
-enemy = Enemy(waypoints, enemy_image)
+enemy = Enemy(world.waypoints, enemy_image)
 enemy_groups.add(enemy)
 
 #game loop
@@ -41,8 +51,11 @@ while run:
     #screen fill
     screen.fill("grey100")
 
+    #draw level
+    world.draw(screen)
+
     #enemy path
-    pgm.draw.lines(screen, "grey0", False, waypoints)
+    pgm.draw.lines(screen, "grey0", False, world.waypoints)
 
     #update groups
     enemy_groups.update()
