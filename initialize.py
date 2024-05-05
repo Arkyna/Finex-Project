89f399f1d312440cebef7b8e9dec734684187c7a
ArkynaSource <jhoelhex6@gmail.com>
2024-05-05 16:40:06 +0700

    tidying up

--- initialize.py
+++ initialize.py
@@ -21,7 +21,14 @@ enemy_image = pgm.image.load('assets\images\monsters\enemy1.png').convert_alpha(
 #creating groups
 enemy_groups = pgm.sprite.Group()
 
-enemy = Enemy((200, 300), enemy_image)
+waypoints = [
+    (100, 100),
+    (250, 250),
+    (280, 180),
+    (90, 200)
+]
+
+enemy = Enemy(waypoints, enemy_image)
 enemy_groups.add(enemy)
 
 #game loop
@@ -31,7 +38,16 @@ while run:
     # limits FPS to 60
     clock.tick(FPS) 
 
+    #screen fill
     screen.fill("grey100")
+
+    #enemy path
+    pgm.draw.lines(screen, "grey0", False, waypoints)
+
+    #update groups
+    enemy_groups.update()
+
+
     #draw groups
     enemy_groups.draw(screen)
 
