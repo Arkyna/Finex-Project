2922e164c840037dab3175ad695f554b3a593575
Eric Arwido Damanik <arwido.eric@gmail.com>
2024-05-06 05:21:35 +0700

    Speed Monster

--- bin/enemy.py
+++ bin/enemy.py
@@ -8,7 +8,7 @@ class Enemy(pgm.sprite.Sprite):
         self.waypoints = waypoints
         self.pos = Vector2(self.waypoints[0])
         self.target_waypoint = 1
-        self.speed = 2
+        self.speed = 1.5
         self.angle = 0
         self.original_image = image
         self.image = pgm.transform.rotate(self.original_image, self.angle)
