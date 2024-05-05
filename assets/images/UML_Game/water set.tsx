aedc23450c5fa93e50c80671d81d15b97cca13cd
Alwi <alwiarfansolin@gmail.com>
2024-05-05 16:39:20 +0700

    update file

---
+++ assets/images/UML_Game/water set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="water set" tilewidth="64" tileheight="64" tilecount="490" columns="70">
+ <image source="D:/Download/IDM/Compressed/Foozle_2DT0012_Spire_Tileset_1/Tileset/PNGs/Animated water tiles.png" width="4480" height="448"/>
+</tileset>
