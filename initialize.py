a6a72dcb3e36479a79e69fedf4c67089eede06ee
ArkynaSource <jhoelhex6@gmail.com>
2024-05-05 15:47:07 +0700

    update enemy_image, enemy_groups

--- initialize.py
+++ initialize.py
@@ -1,5 +1,5 @@
 import pygame as pgm
-from bin import Enemy
+from bin.enemy import Enemy
 
 #initialisasi
 pgm.init()
@@ -16,17 +16,32 @@ screen = pgm.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pgm.display.set_caption("TDEFENSE")
 
 #load image
-enemy_image = pgm.image.load() 
+enemy_image = pgm.image.load('assets\images\monsters\enemy1.png').convert_alpha()
+
+#creating groups
+enemy_groups = pgm.sprite.Group()
+
 enemy = Enemy((200, 300), enemy_image)
+enemy_groups.add(enemy)
 
 #game loop
 run = True
 while run:
+
     # limits FPS to 60
     clock.tick(FPS) 
+
+    screen.fill("grey100")
+    #draw groups
+    enemy_groups.draw(screen)
+
     #event handler
     for event in pgm.event.get():
+            #quit program
         if event.type == pgm.QUIT:
             run = False
+
+    #update display
+    pgm.display.flip()
 
 pgm.quit()
