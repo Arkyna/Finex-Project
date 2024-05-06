8deefee8540a1bf80020e4c42b745be34e7e74e0
ArkynaSource <jhoelhex6@gmail.com>
2024-05-06 08:03:53 +0700

    animation don

--- initialize.py
+++ initialize.py
@@ -12,7 +12,7 @@ pgm.init()
 # create clock
 clock = pgm.time.Clock()
 
-# creating window game
+# creating game window
 screen = pgm.display.set_mode((val.SCREEN_WIDTH + val.SIDE_PANEL, val.SCREEN_HEIGHT))
 pgm.display.set_caption(val.GAME_NAME)
 
@@ -24,6 +24,12 @@ placing_tower = False
 # Load images
 # map
 map_image = pgm.image.load(r'assets\images\map\level1.png').convert_alpha()
+
+# tower sprite sheet
+tower_sheet = pgm.image.load(r'assets\images\towers\weapon_heavy_arrow.png').convert_alpha()
+
+# tower sprite below tower sheet
+base_tower = pgm.image.load(r'assets\images\towers\tower1.png').convert_alpha()
 
 # individual tower image for mouse cursor
 cursor_tower = pgm.image.load(r'assets\images\towers\tower1.png').convert_alpha()
@@ -38,10 +44,11 @@ cancel_button_image = pgm.image.load(r'assets\images\buttons\cancel_button.png')
 # sidebar 
 sidebar_image = pgm.image.load(r'assets\images\gui\sidepanel.png').convert_alpha()
 
-# load json data for level
+# load json data for enemy path in levels
 with open('bin\levels\level1.tmj') as file:
     world_data = json.load(file)
 
+# audio
 pgm.mixer.music.load(r"assets\audios\bgm2.mp3")
 pgm.mixer.music.play()
 
@@ -63,8 +70,9 @@ def create_tower(mouse_pos):
                 space_is_free = False
         # if free then place tower
         if space_is_free == True:
-            new_tower = Tower(cursor_tower, mouse_tile_x, mouse_tile_y)
+            new_tower = Tower(base_tower, tower_sheet, mouse_tile_x, mouse_tile_y)
             tower_groups.add(new_tower)
+
 
 # create world
 world = World(world_data, map_image)
@@ -89,12 +97,13 @@ while run:
     # limits FPS to 60
     clock.tick(val.FPS) 
 
-     #########################
+    #########################
     # UPDATING SECTION
     #########################
 
     #update groups
     enemy_groups.update()
+    tower_groups.update()
 
     #########################
     # DRAWING SECTION
@@ -110,6 +119,12 @@ while run:
     pgm.draw.lines(screen, "grey0", False, world.waypoints)
 
     #draw groups
+    for tower in tower_groups:
+        # Draw tower base
+        screen.blit(tower.base_tower, tower.base_rect)
+        # Draw tower animation frame
+        screen.blit(tower.image, tower.rect)
+
     enemy_groups.draw(screen)
     tower_groups.draw(screen)
 
@@ -131,8 +146,7 @@ while run:
 
         if cancel_button.draw(screen):
             placing_tower = False
-
-    # print(tower_groups)
+            #print(tower_groups)
 
     #event handler
     for event in pgm.event.get():
